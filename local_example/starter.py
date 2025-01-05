c4cb2eb03eb774c8e8ed0663b7aa9dd71f1d89fd
EdFage <87755165+EdFage@users.noreply.github.com>
2025-01-05 11:07:40 +0000

    add command line arguments

--- local_example/starter.py
+++ local_example/starter.py
@@ -1,7 +1,16 @@
+import sys
+
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings, StorageContext, load_index_from_storage
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.ollama import Ollama
 
+if len(sys.argv) < 2:
+    print("Usage: python starter.py <query>")
+    sys.exit(1)
+
+query = sys.argv[1]
+
+# Load documents
 documents = SimpleDirectoryReader("data").load_data()
 
 # bge-base embedding model
@@ -17,5 +26,5 @@ storage_context = StorageContext.from_defaults(persist_dir="data/indexed_data")
 index = load_index_from_storage(storage_context)
 
 query_engine = index.as_query_engine()
-response = query_engine.query("What did the author do growing up?")
+response = query_engine.query(query)
 print(response)
