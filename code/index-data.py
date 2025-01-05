cbe00285efa48cddde0762005530b61dcd15ca38
EdFage <87755165+EdFage@users.noreply.github.com>
2025-01-05 14:57:47 +0000

    make indexing recursive

--- code/index-data.py
+++ code/index-data.py
@@ -6,7 +6,7 @@ import logging
 import sys
 
 # Load documents
-documents = SimpleDirectoryReader("data").load_data()
+documents = SimpleDirectoryReader("data", recursive=True, required_exts=[".pdf", ".docx"]).load_data()
 
 # bge-base embedding model
 Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
