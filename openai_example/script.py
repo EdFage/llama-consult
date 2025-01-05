6de820accd75c1c723f12c7e448a9862b7044453
EdFage <87755165+EdFage@users.noreply.github.com>
2025-01-05 11:58:46 +0000

    remove openai_example

--- openai_example/script.py
+++
@@ -1,7 +0,0 @@
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
-
-documents = SimpleDirectoryReader("data").load_data()
-index = VectorStoreIndex.from_documents(documents)
-query_engine = index.as_query_engine()
-response = query_engine.query("where can i find info about china's coal demand? please give me the page number and filename")
-print(response)
