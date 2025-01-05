92157a9c1ef2d0e361a6747f847e523e87cb7956
EdFage <87755165+EdFage@users.noreply.github.com>
2025-01-05 10:19:43 +0000

    update

--- openai_example/script.py
+++ openai_example/script.py
@@ -3,5 +3,5 @@ from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 documents = SimpleDirectoryReader("data").load_data()
 index = VectorStoreIndex.from_documents(documents)
 query_engine = index.as_query_engine()
-response = query_engine.query("What's the key takeaways abut the world energy market? Please explain where you get the info from, referring to the 'data' folder")
+response = query_engine.query("where can i find info about china's coal demand? please give me the page number and filename")
 print(response)
